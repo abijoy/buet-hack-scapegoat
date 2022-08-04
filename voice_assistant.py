96541cdac9647168be822d4861a26506cff33e95
Shovon Karmkaer <51527801+shuvo1997@users.noreply.github.com>
2022-08-04 22:49:06 +0600

    Adds artist, track and genre features

--- voice_assistant.py
+++ voice_assistant.py
@@ -34,6 +34,7 @@ def wishMe():
 
 def takeCommand():
     r = sr.Recognizer()
+    r.pause_threshold = 0.8
     with sr.Microphone() as source:
         print("Listening...")
         audio = r.listen(source)
@@ -48,24 +49,57 @@ def takeCommand():
         return statement
 
 
+def sayGoodbye():
+    speak('your personal assistant G-one is shutting down,Good bye')
+    print('your personal assistant G-one is shutting down,Good bye')
+
+
+def searchWikipedia(statement):
+    speak('Searching Wikipedia...')
+    statement = statement.replace("wikipedia", "")
+    results = wikipedia.summary(statement, sentences=3)
+    speak("According to Wikipedia")
+    print(results)
+    speak(results)
+
+
+genres = ['pop', 'hip hop', 'rap', 'rock',
+          'electronic', 'latin', 'indie', 'classical', 'k pop', 'country', 'metal', 'romantic']
+
 if __name__ == '__main__':
 
     while True:
         speak("Tell me how can I help you now?")
         statement = takeCommand().lower()
-        # Constantly listen for commands
-        if statement == 0:
+        try:
+            # Constantly listen for commands
+            if statement == 0:
+                continue
+            # Say these words for shut the assistant down
+            if "goodbye" in statement or "ok bye" in statement or "stop" in statement:
+                sayGoodbye()
+                break
+            # Search wikipedia
+            if 'what is' in statement or 'wikipedia' in statement:
+                searchWikipedia(statement)
+            if 'artist' in statement:
+                speak('Say name of the artist')
+                statement = takeCommand().lower()
+                print(statement)
+                print('searching artist')
+                speak('searching artist')
+            for genre_name in genres:
+                if genre_name in statement:
+                    speak(f'searching {genre_name} song')
+                    print(f'searching {genre_name} song')
+            if 'track' in statement or 'search song' in statement:
+                speak('Say name of the track')
+                statement = takeCommand().lower()
+                print(statement)
+                print('searching track')
+                speak('searching track')
+
+        except Exception as e:
+            print("sorry no such result..please try again..")
+            speak('sorry no such result. please try again...')
             continue
-        # Say these words for shut the assistant down
-        if "goodbye" in statement or "ok bye" in statement or "stop" in statement:
-            speak('your personal assistant G-one is shutting down,Good bye')
-            print('your personal assistant G-one is shutting down,Good bye')
-            break
-        # Search wifipedia
-        if 'wikipedia' in statement:
-            speak('Searching Wikipedia...')
-            statement = statement.replace("wikipedia", "")
-            results = wikipedia.summary(statement, sentences=3)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)

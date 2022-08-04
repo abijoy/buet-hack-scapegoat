aaf42f330f7fa4951e401da89930d4f373460883
Shovon Karmkaer <51527801+shuvo1997@users.noreply.github.com>
2022-08-04 17:52:09 +0600

    Working voice assistant with wiki search

---
+++ voice_assistant.py
@@ -0,0 +1,71 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import os
+import time
+import subprocess
+import json
+import requests
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'voices[0].id')
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = datetime.datetime.now().hour
+    if hour >= 0 and hour < 12:
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif hour >= 12 and hour < 18:
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
+    else:
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+        try:
+            statement = r.recognize_google(audio, language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
+
+
+if __name__ == '__main__':
+
+    while True:
+        speak("Tell me how can I help you now?")
+        statement = takeCommand().lower()
+        # Constantly listen for commands
+        if statement == 0:
+            continue
+        # Say these words for shut the assistant down
+        if "goodbye" in statement or "ok bye" in statement or "stop" in statement:
+            speak('your personal assistant G-one is shutting down,Good bye')
+            print('your personal assistant G-one is shutting down,Good bye')
+            break
+        # Search wifipedia
+        if 'wikipedia' in statement:
+            speak('Searching Wikipedia...')
+            statement = statement.replace("wikipedia", "")
+            results = wikipedia.summary(statement, sentences=3)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)

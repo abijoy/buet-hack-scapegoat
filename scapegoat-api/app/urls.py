1dcef6ecf5fff79d086f30814457994610364874
abijoy <mdalaminbijoy3@gmail.com>
2022-08-05 09:18:39 +0600

    added api backend

---
+++ scapegoat-api/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('news/top-news', views.get_top_news, name='get_top_news'),
+    path('news/geo-top-news', views.get_geo_top_news, name='get_geo_top_news'),
+    path('music/recommendation', views.get_music_recom_track, name='get_music_recom_track'),
+    path('route/planning', views.getDirection, name="getDirection"),
+]
